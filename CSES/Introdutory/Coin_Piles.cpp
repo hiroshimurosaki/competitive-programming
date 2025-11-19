805f979a7a8408a6b53f8c2961d163cec3b4d278
Fernando Hiroshi Murusaki <nandomurosakii@gmail.com>
2025-11-19 14:38:54 -0300

    teste

---
+++ CSES/Introdutory/Coin_Piles.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int t;
+    int a, b;
+    cin >> t;
+
+    for(int i=0; i<t;i++){
+        cin >> a >> b;
+
+        if(((a <= 1) && (b <= 1)) || (b == 0) || (a == 0)){
+            cout << "NO" << endl;
+        } 
+        
+        else if(a%2==0){
+            if(b%2==0){
+                cout << "NO"<< endl;
+            } else {
+                cout << "YES"<< endl;
+            }
+        } else{
+            cout << "YES" << endl;
+        }
+
+    }
+};
