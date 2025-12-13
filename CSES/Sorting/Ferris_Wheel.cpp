88ce08de731f959f58e31df0dd3a82198b52fed8
Fernando Hiroshi Murusaki <nandomurosakii@gmail.com>
2025-12-13 13:02:18 -0300

    foi

--- CSES/Sorting/Ferris_Wheel.cpp
+++ CSES/Sorting/Ferris_Wheel.cpp
@@ -5,32 +5,35 @@ using namespace std;
 int main(){
  
     int n, x;
-    vector<int> c;
-    map<int, int> g;
     cin >> n >> x;
+    vector<int> c(n);
     int soma = 0;
 
     for(int i=0; i<n; i++){
 
-        int aux;
-        cin >> aux;
-        int temp = x-aux;
-        c.push_back(aux);
+        cin >> c[i];
 
-        if(!g[temp]){
-            g[temp] += 1;
-            soma += 1;
-        } else {
-            g[temp] -= 1;
+    }
+
+    sort(c.begin(), c.end());
+
+    int i = 0;          
+    int j = n - 1;      
+
+    while(i <= j) {
+
+        if(c[i] + c[j] <= x) {
+            i++; 
+            j--; 
+        } 
+        else {
+
+            j--;
         }
 
+        soma++;
     }
 
-    for(auto it = g.begin(); it!= g.end(); it++){
-        cout << it->first <<  " " << it->second << endl;
-    }
-
-
-    
+    cout << soma << endl;
 
 }
